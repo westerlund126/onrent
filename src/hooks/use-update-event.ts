3c70f677dcce74261155ba05abbed6b66422dc96
Divara Fathsel <sevenzeroseven.notfound@gmail.com>
2025-06-25 18:54:52 +0700

    fix (owner): calendar

--- src/hooks/use-update-event.ts
+++ src/hooks/use-update-event.ts
@@ -1,19 +1,19 @@
 import { useCalendar } from 'contexts/calendar-context';
 
-import type { IEvent } from 'types/fitting';
+import type { IFittingSchedule } from 'types/fitting';
 
 export function useUpdateEvent() {
-  const { setLocalEvents } = useCalendar();
+  const { setLocalSchedule } = useCalendar();
 
   // This is just and example, in a real scenario
   // you would call an API to update the event
-  const updateEvent = (event: IEvent) => {
-    const newEvent: IEvent = event;
+  const updateEvent = (event: IFittingSchedule) => {
+    const newEvent: IFittingSchedule = event;
 
-    newEvent.startDate = new Date(event.startDate).toISOString();
-    newEvent.endDate = new Date(event.endDate).toISOString();
+    newEvent.startTime = new Date(event.startTime);
+    newEvent.endTime = new Date(event.endTime);
 
-    setLocalEvents((prev) => {
+    setLocalSchedule((prev) => {
       const index = prev.findIndex((e) => e.id === event.id);
       if (index === -1) return prev;
       return [...prev.slice(0, index), newEvent, ...prev.slice(index + 1)];
