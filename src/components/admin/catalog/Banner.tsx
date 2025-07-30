c3dccdd93713006c4ff7e25bca91ba604cab31fe
westerlund126 <sevenzeroseven.notfound@gmail.com>
2025-07-30 23:36:52 +0700

    minor fix

--- src/components/admin/catalog/Banner.tsx
+++ src/components/admin/catalog/Banner.tsx
@@ -8,7 +8,7 @@ const Banner1 = () => {
     >
       <div className="w-full">
         <h4 className="mb-[14px] max-w-full text-xl font-bold text-white md:w-[64%] md:text-3xl md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
-          Discover and Rent extraordinary Dresses
+          Pakaian Menawan untuk Setiap Perayaan
         </h4>
         {/* <p className="mb-[40px] max-w-full text-base font-medium text-[#E3DAFF] md:w-[64%] lg:w-[40%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
           Enter in this creative world. Discover now the latest NFTs or start
